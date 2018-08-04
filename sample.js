979e0c906b78b51a1b0f2bb28907566aed72c866
SandeshOnRails <basnetsandesh450@gmail.com>
2018-08-04 01:24:56 -0700

    best value algorithm added

--- sample.js
+++ sample.js
@@ -52,7 +52,24 @@ app.post("/search", function(req, res){
 	client.search(data).then(function(response){
 		
 		let results = response.jsonBody.businesses;
-	    res.render("results", {results: results});
+     
+     console.log(typeof response.jsonBody.businesses[0].price)
+    console.log(typeof response.jsonBody.businesses[0].rating)
+
+    var sorted_results = bestValue(results)
+
+
+    for (var i =0; i < sorted_results.length; i++){
+
+      console.log(sorted_results[i].name)
+
+          console.log(sorted_results[i].price)
+
+          console.log(sorted_results[i].rating)
+    }
+
+
+	    res.render("results", {results: sorted_results});
 
 	}).catch(function(err){
 	console.log(err);
@@ -65,14 +82,14 @@ app.get("/search/reviews/:id", function(req, res){
 	
 const businessId = req.params.id.substring(1);
 
-console.log(req.params.id);
+//console.log(req.params.id);
 
 
  client.reviews(businessId).then(function(response){
  
       const revs = response.jsonBody.reviews;
 
-      console.log(revs[0].text);
+      //console.log(revs[0].text);
 
 
       res.render("reviews.ejs", {revs: revs});
@@ -87,3 +104,53 @@ app.listen(5000, function(){
 
 	console.log("Server Running");
 })
+
+
+function bestValue (results) {
+
+        for(var i = 0; i < results.length; i ++ ) {
+               
+
+               console.log(results[i].price)
+               console.log(results[i].rating)
+
+         for (var j = 0; j < results.length -1 -i; j++) {
+
+
+              if((results[j].price.length + results[j].rating)/2 < (results[j+1].price.length + results[j+1].rating)/2 ) {
+                            
+                            const lesserValue = results[j]
+
+                          results[j] = results[j+1]
+
+                          results[j+1] = lesserValue
+
+              }
+         }
+
+    
+}
+  
+  return results
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
